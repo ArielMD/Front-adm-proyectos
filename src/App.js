e47ad8775b20824658c2337a33c89bd8fd467961
Ariel May <aemd199711@gmail.com>
2020-07-30 21:24:15 -0500

    feat: vistas principales y rutas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./views/home/Home";
+import Login from "./views/login/Login";
+import Register from "./views/register/Register";
 
 function App() {
-  return <div>Hola</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Login}></Route>
+        <Route exact path="/nuevaCuenta" component={Register}></Route>
+        <Route exact path="/proyectos" component={Home}></Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
