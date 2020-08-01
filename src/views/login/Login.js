fb23848227e32c0b2a065b53bd741d34f80b24b2
Ariel May <aemd199711@gmail.com>
2020-07-31 23:06:53 -0500

    feat: login y validaciones

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,6 +1,63 @@
 import React from "react";
+import LoginStyled from "./loginStyled";
+import { useForm } from "react-hook-form";
+import iconUser from "../../assets/usuario.svg";
+import iconPadlock from "../../assets/candado.svg";
+import logoUser from "../../assets/logoUsuario.svg";
+
 const Login = () => {
-  return <h1>login</h1>;
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  const EMAIL = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+  return (
+    <LoginStyled>
+      <div className="container">
+        <div className="wrapper">
+          <img className="logo" src={logoUser}></img>
+          <h1>Iniciar Sessión</h1>
+          <form className="form-user" onSubmit={handleSubmit(onSubmit)}>
+            <div className="input-group">
+              <label className="input">
+                <img src={iconUser}></img>
+                <input
+                  type="text"
+                  placeholder="Nombre de usuario"
+                  name="username"
+                  ref={register({
+                    required: "Campo requerido",
+                    pattern: {
+                      value: EMAIL,
+                      message: "Debes ingresar un correo",
+                    },
+                  })}
+                ></input>
+              </label>
+              {errors.username && <small>{errors.username.message}</small>}
+            </div>
+            <div className="input-group">
+              <label className="input">
+                <img src={iconPadlock}></img>
+                <input
+                  type="password"
+                  placeholder="password"
+                  name="password"
+                  ref={register({
+                    required: "Campo requerido",
+                  })}
+                ></input>
+              </label>
+              {errors.password && <small>{errors.password.message}</small>}
+            </div>
+            <button className>Entrar</button>
+          </form>
+        </div>
+      </div>
+    </LoginStyled>
+  );
 };
 
 export default Login;
